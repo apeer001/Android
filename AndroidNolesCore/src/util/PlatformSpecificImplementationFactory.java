6d401741231b19a34d72742dac20d9860df4db9f
Jonathan Steele <xfsunoles.com>
2011-08-25 15:42:04 -0400

    Change license back to Google and add ternary opetator.

--- AndroidNolesCore/src/util/PlatformSpecificImplementationFactory.java
+++ AndroidNolesCore/src/util/PlatformSpecificImplementationFactory.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2011 Jonathan Steele
+ * Copyright 2011 Google Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.itnoles.shared.util;
@@ -35,10 +35,7 @@ public class PlatformSpecificImplementationFactory
      */
     public static IStrictMode getStrictMode()
     {
-        if (SportsConstants.SUPPORTS_GINGERBREAD) {
-            return new GingerbreadStrictMode();
-        }
-        return null;
+        return SportsConstants.SUPPORTS_GINGERBREAD ? new GingerbreadStrictMode() : null;
     }
 
     /**
@@ -48,12 +45,9 @@ public class PlatformSpecificImplementationFactory
      */
     public static SharedPreferenceSaver getSharedPreferenceSaver(Context ctx)
     {
-        if (SportsConstants.SUPPORTS_GINGERBREAD) {
-            return new GingerbreadSharedPreferenceSaver(ctx);
-        }
-        else if (SportsConstants.SUPPORTS_FROYO) {
-            return new FroyoSharedPreferenceSaver(ctx);
-        }
-        return new LegacySharedPreferenceSaver(ctx);
-    }
+        return SportsConstants.SUPPORTS_GINGERBREAD ?
+            new GingerbreadSharedPreferenceSaver(ctx) :
+            SportsConstants.SUPPORTS_FROYO ?
+            new GingerbreadSharedPreferenceSaver(ctx) :
+            new LegacySharedPreferenceSaver(ctx);
 }
