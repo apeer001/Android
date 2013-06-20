ef6e9139aa184e9afd1dd73a61fe1e75e8aac1a8
Jonathan Steele <xfsunoles.com>
2013-06-20 02:06:19 -0400

    some how collegesportsflavor didn't get added.

---
+++ collegesportsflavor/src/main/java/com/itnoles/flavored/MainActivity.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2013 Jonathan Steele
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.itnoles.flavored;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.view.ViewPager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import com.itnoles.flavored.fragment.HeadlinesFragment;
+import com.itnoles.flavored.fragment.RostersFragment;
+//import com.itnoles.flavored.fragment.ScheduleFragment;
+
+public class MainActivity extends Activity implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) {
+            StrictMode.enableDefaults();
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        VolleyHelper.getInstance(this);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new HomePagerAdapter(getFragmentManager()));
+        mViewPager.setOnPageChangeListener(this);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("News").setTabListener(this));
+        //actionBar.addTab(actionBar.newTab().setText("Schedule").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Rosters").setTabListener(this));
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        getActionBar().setSelectedNavigationItem(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+    }
+
+    private class HomePagerAdapter extends FragmentPagerAdapter {
+        public HomePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new HeadlinesFragment();
+                case 1:
+                    //return new ScheduleFragment();
+                //case 2:
+                    return new RostersFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+}
---
+++ collegesportsflavor/src/main/java/com/itnoles/flavored/XMLRequest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2013 Jonathan Steele
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.itnoles.flavored;
+
+import android.util.Log;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+public class XMLRequest extends Request<XmlPullParser> {
+	private static final String LOG_TAG = "XMLRequest";
+
+    private static XmlPullParserFactory sXmlPullParserFactory;
+    static {
+        try {
+            sXmlPullParserFactory = XmlPullParserFactory.newInstance();
+        } catch (XmlPullParserException e) {
+            Log.e(LOG_TAG, "Could not instantiate XmlPullParserFactory", e);
+        }
+    }
+
+    private final Response.Listener<XmlPullParser> mListener;
+
+	public XMLRequest(String url, Response.Listener<XmlPullParser> listener) {
+		super(Method.GET, url, new Response.ErrorListener() {
+			@Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v(LOG_TAG, "VolleyError", error);
+            }
+		});
+        mListener = listener;
+	}
+
+    @Override
+    protected void deliverResponse(XmlPullParser response) {
+        mListener.onResponse(response);
+    }
+
+    @Override
+    protected Response<XmlPullParser> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String xmlString = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+            XmlPullParser parser = sXmlPullParserFactory.newPullParser();
+            parser.setInput(new StringReader(xmlString));
+            return Response.success(parser, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (XmlPullParserException xmle) {
+            return Response.error(new ParseError(xmle));
+        }
+    }
+}
