06059f2416062e6e6744d37e17d0866b9db4e6b2
Jonathan Steele <iNoles@users.noreply.github.com>
2013-09-07 01:52:59 -0400

    Fix Copyright.

--- CollegeSportsFlavor/src/main/java/com/itnoles/flavored/Predicate.java
+++ CollegeSportsFlavor/src/main/java/com/itnoles/flavored/Predicate.java
@@ -13,6 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+/**
+ * Original Source in http://java.dzone.com/articles/getting-rid-boilerplate-code
+ */
 
 package com.itnoles.flavored;
 
